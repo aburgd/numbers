cc241376325fdbaba8bc09e6d2c98270c776ebae
Alec Burgdorf <aeburgd@gmail.com>
2017-04-25 17:54:12 -0500

    just rewrite most of it, add some functions

--- numbers.py
+++ numbers.py
@@ -1,19 +1,41 @@
 from gtts import gTTS
 from os import urandom
 from sys import byteorder
+from time import sleep
+import tweepy
+import settings
 
 
-def ten_num():
+cons_key = settings.CONSUMER_KEY
+cons_sec = settings.CONSUMER_SECRET
+acc_key = settings.ACCESS_KEY
+acc_sec = settings.ACCESS_SECRET
+
+
+def tweepy_setup(cons_key, cons_sec, acc_key, acc_sec):
+    auth = tweepy.OAuthHandler(cons_key, cons_sec)
+    auth.set_access_token(acc_key, acc_sec)
+    api = tweepy.API(auth)
+    return api
+
+
+def big_number():
     num_l = []
     for i in range(1, 5):
         n_bytes = urandom(8)
         num = int.from_bytes(n_bytes, byteorder)
-        num_l.insert(0, num)
-    return num_l
+        num_l.insert(0, str(num))
+    return "".join(num_l)
 
-list_nums = ten_num()
-list_nums = [str(x) for x in list_nums]
-list_nums = " ".join(list_nums)
 
-tts = gTTS(list_nums, slow=True)
-tts.save("god.mp3")
+def tweet(api, tweet):
+    api.update_status(tweet)
+    sleep(86400)
+
+try:
+    print("Running")
+    tweepy_setup(cons_key, cons_sec, acc_key, acc_sec)
+    number = big_number()
+    tweet(api, number)
+except KeyboardInterrupt:
+    print("WARN - Stopping")
